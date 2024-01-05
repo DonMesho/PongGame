7de7ca45a1ffc777d0ebc578bbe23403d3460302
Mesho <148106010+DonMesho@users.noreply.github.com>
2024-01-05 23:21:56 +0200

    Add files via upload

---
+++ Project2/Project2/Paddle.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+namespace Project2 { 
+    public class Paddle
+    {
+        bool isSecondPlayer;
+        public Paddle(bool isSecondPlayer)
+        {
+            this.isSecondPlayer = isSecondPlayer;
+            rect = new Rectangle((this.isSecondPlayer ? Globals.WIDTH - 40 : 0), 140, 40, 200);
+        }
+
+        public Rectangle rect;
+        float moveSpeed = 500f;
+
+        public Paddle() {
+            rect = new Rectangle(0, 140, 40, 200);
+
+                }
+        public void Update(GameTime gameTime)
+        {
+            KeyboardState kstate = Keyboard.GetState();
+            if ((this.isSecondPlayer ? kstate.IsKeyDown(Keys.Up) : kstate.IsKeyDown(Keys.W)) && rect.Y > 0)
+            {
+                rect.Y -= (int)(moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+            }
+            if ((this.isSecondPlayer ? kstate.IsKeyDown(Keys.Down) : kstate.IsKeyDown(Keys.S)) && rect.Y < Globals.HEIGHT - rect.Height)
+            {
+                rect.Y += (int)(moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+            }
+        }
+
+
+        public static void update(GameTime gameTime) { }
+        public void Draw() {
+            Globals.spriteBatch.Draw(Globals.pixel, rect, Color.White);
+            }
+        }
+    }
