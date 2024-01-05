7f8f173fd48c419521ae60a6e55aa57db10abf28
Mesho <148106010+DonMesho@users.noreply.github.com>
2024-01-05 23:26:23 +0200

    Add files via upload

---
+++ Project2/Globals.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Globals
+    {
+        public static SpriteBatch spriteBatch;
+        public static int WIDTH = 640, HEIGHT = 480;
+        public static Texture2D pixel;
+        public static int player1_score, player2_score;
+    }
+}
